be63d8aa54072ccbcedbcfa0c5672d3b94b6659e
shinyay <shinya.com@gmail.com>
2019-08-17 23:17:07 +0900

    Add LoadBalancer annotation
    Configured custom loadbalancer configuration class

--- src/main/kotlin/io/pivotal/shinyay/greenwich/frontend/FrontendApplication.kt
+++ src/main/kotlin/io/pivotal/shinyay/greenwich/frontend/FrontendApplication.kt
@@ -2,9 +2,18 @@ package io.pivotal.shinyay.greenwich.frontend
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cache.annotation.EnableCaching
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient
+import org.springframework.context.annotation.Bean
+import org.springframework.web.client.RestTemplate
 
 @SpringBootApplication
-class FrontendApplication
+@LoadBalancerClient(name = "backend-service", configuration = [ClientConfiguration::class])
+@EnableCaching
+class FrontendApplication {
+	@Bean
+	fun restTemplate() = RestTemplate()
+}
 
 fun main(args: Array<String>) {
 	runApplication<FrontendApplication>(*args)
