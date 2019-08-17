8b09218425b077d693f6c8c80f321c922a59cfb8
shinyay <shinya.com@gmail.com>
2019-08-17 18:24:00 +0900

    Initial Commit

---
+++ src/main/kotlin/io/pivotal/shinyay/greenwich/frontend/FrontendApplication.kt
@@ -0,0 +1,11 @@
+package io.pivotal.shinyay.greenwich.frontend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FrontendApplication
+
+fun main(args: Array<String>) {
+	runApplication<FrontendApplication>(*args)
+}
